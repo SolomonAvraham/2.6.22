14bf6b9c56047f6be4bb9780785d93d2601e9b36
SolomonAvraham <Selmon_a@hotmail.com>
2022-06-04 15:49:20 +0300

    update

--- main.js
+++ main.js
@@ -1,34 +1,39 @@
 function fullNameInput() {
-  var fullNameInputData = document.getElementById("one");
-  fullNameInputData.addEventListener("change", fullNameInput);
-  fullNameInputData.innerText = "";
-  fullNameInputData.style = "color:blue;";
-  var result = fullNameInputData.value;
-
-  return result;
+  var x = document.getElementById("first_input");
+  x.style = "color:blue;";
+  return x.value;
 }
 
 function yearInput() {
-  var yearInputData = document.getElementById("two");
+  var yearInputData = document.getElementById("second_input");
   yearInputData.style = "color:blue;";
-  var result = yearInputData.value;
-  return result;
+  return yearInputData.value;
 }
 
-function firstButtonData() {
-  var firstButton = document.getElementById("firstButton");
-  firstButton.onclick = () => {
-    if (yearInput() < 2004) {
-      var messageAlert = document.getElementById("alert_message");
-      messageAlert.innerHTML = "<h1>WELCOME!</h1>";
-      messageAlert.style = "color:blue;text-align: center;";
-    } else {
-      var messageAlert = document.getElementById("alert_message");
-      messageAlert.innerHTML =
-        "<h1>NO ENTRY!</h1>" + "<img src=" + "PIC.png" + " >";
-      messageAlert.style = "color:red;text-align: center;";
-    }
-    return messageAlert;
-  };
+function cleartext() {
+  document.getElementById("first_input").value = "";
+  document.getElementById("second_input").value = "";
 }
-firstButtonData();
+
+document
+  .getElementById("firstButton")
+  .addEventListener("click", firstButtonData);
+
+function firstButtonData() {
+  var messageAlert = document.getElementById("alert_message");
+
+  if (yearInput() < 2004) {
+    messageAlert.innerHTML = "<br>" + "<h1>WELCOME!</h1>";
+    messageAlert.style = "color:blue;text-align: center;";
+    messageAlert.onmouseover = () => {
+      messageAlert.style = "color:green;text-align: center;";
+    };
+  } else {
+    messageAlert.onmouseover = () => {};
+    messageAlert.innerHTML =
+      "<br>" + "<h1>NO ENTRY!</h1>" + "<img src=" + "PIC.png" + " >";
+    messageAlert.style = "color:red;text-align: center;";
+  }
+
+  cleartext();
+}
